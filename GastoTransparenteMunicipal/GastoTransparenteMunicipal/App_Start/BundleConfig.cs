4bf7756b9f44de5b34727fd8784ba6da69b1ef92
Luis Miguel Leon Valenzuela <luiscleverboy@gmail.com>
2018-01-15 15:59:20 -0300

    Optimizacion de CSS
    *Imagenes
    *Logo.

--- GastoTransparenteMunicipal/GastoTransparenteMunicipal/App_Start/BundleConfig.cs
+++ GastoTransparenteMunicipal/GastoTransparenteMunicipal/App_Start/BundleConfig.cs
@@ -10,17 +10,10 @@ namespace GastoTransparenteMunicipal
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
+            
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/bootstrap-reboot.css",
                       "~/Content/css/bootstrap-grid.css",
