c19dfbd02eee2a4bc8332e2ef26a4c231075aacf
Luis Miguel Leon Valenzuela <lmleon19@gmail.com>
2018-05-04 18:16:14 -0300

    Dinamismo en Menu
    Menu Cambios.

--- GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
+++ GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using GastoTransparenteMunicipal.Models;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Core;
+using Newtonsoft.Json;
 
 namespace GastoTransparenteMunicipal.Controllers
 {
-    public class AdminComunaController : Controller
+    public class AdminComunaController : BaseController
     {
         public ActionResult Index()
         {
             return View();
         }
-
-
+        
         public ActionResult CargaDatos()
         {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.Anos = new SelectList(db.Gasto_Ano.Where(r => r.IdMunicipalidad == municipalidad.IdMunicipalidad), "IdAno", "Nombre");
             return View();
         }
     }
