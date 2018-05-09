5e13be41cbbc352671a5ae410176a47e43d0f241
Luis Miguel Leon Valenzuela <lmleon19@gmail.com>
2018-05-09 15:41:20 -0300

    Vista de Carga de Info.
    para subir los dataSet la vista para cargar ese tipo de contenido.

--- GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
+++ GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Core;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace GastoTransparenteMunicipal.Controllers
 {
@@ -15,11 +16,184 @@ namespace GastoTransparenteMunicipal.Controllers
         {
             return View();
         }
-        
+
         public ActionResult CargaDatos()
         {
             var municipalidad = GetCurrentIdMunicipality();
-            ViewBag.Anos = new SelectList(db.Gasto_Ano.Where(r => r.IdMunicipalidad == municipalidad.IdMunicipalidad), "IdAno", "Nombre");
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.Anos = new SelectList(db.Anos_Invisible.Where(r => r.IdMunicipalidad == municipalidad.IdMunicipalidad), "IdAno", "Nombre");
+            return View();
+        }
+
+        public JsonResult CargadosPost(int aux)
+        {
+            Gasto_Ano gasto = db.Gasto_Ano.Find(aux);
+            List<bool> lista = new List<bool>
+            {
+                gasto.Cargado,
+                db.Ingreso_Ano.Any(r=>r.Ano==gasto.Ano && r.Semestre==gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad && r.Cargado==true),
+                db.Proveedor_Ano.Any(r=>r.Ano==gasto.Ano && r.Semestre==gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad && r.Cargado==true),
+                db.Subsidio_Ano.Any(r=>r.Ano==gasto.Ano && r.Semestre==gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad && r.Cargado==true),
+                db.Corporacion_Ano.Any(r=>r.Ano==gasto.Ano && r.Semestre==gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad && r.Cargado==true),
+                db.Personal_Ano.Any(r=>r.Ano==gasto.Ano && r.Semestre==gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad && r.Cargado==true)
+            };
+            var auxiliar = lista;
+            return Json(auxiliar, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CargaIngresos(int id)
+        {         
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            Ingreso_Ano ingr = db.Ingreso_Ano.First(r => r.Ano == gasto.Ano && r.Semestre == gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad);
+            ViewBag.aviso = ingr.Cargado;
+            switch(ingr.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = ingr.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = ingr.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = ingr.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = ingr.Ano;
+                    break;
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CargaIngresos(HttpPostedFileBase file)
+        {
+            return View();
+        }
+
+        public ActionResult CargaGastos(int id)
+        {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            ViewBag.aviso = gasto.Cargado;
+            switch (gasto.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = gasto.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = gasto.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = gasto.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = gasto.Ano;
+                    break;
+            }
+            return View();
+        }
+        public ActionResult CargaProveedores(int id)
+        {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            Proveedor_Ano ingr = db.Proveedor_Ano.First(r => r.Ano == gasto.Ano && r.Semestre == gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad);
+            ViewBag.aviso = ingr.Cargado;
+            switch (ingr.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = ingr.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = ingr.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = ingr.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = ingr.Ano;
+                    break;
+            }
+            return View();
+        }
+        public ActionResult CargaSubsidios(int id)
+        {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            Subsidio_Ano ingr = db.Subsidio_Ano.First(r => r.Ano == gasto.Ano && r.Semestre == gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad);
+            ViewBag.aviso = ingr.Cargado;
+            switch (ingr.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = ingr.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = ingr.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = ingr.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = ingr.Ano;
+                    break;
+            }
+            return View();
+        }
+        public ActionResult CargaCorporaciones(int id)
+        {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            Corporacion_Ano ingr = db.Corporacion_Ano.First(r => r.Ano == gasto.Ano && r.Semestre == gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad);
+            ViewBag.aviso = ingr.Cargado;
+            switch (ingr.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = ingr.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = ingr.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = ingr.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = ingr.Ano;
+                    break;
+            }
+            return View();
+        }
+        public ActionResult CargaRemuneraciones(int id)
+        {
+            var municipalidad = GetCurrentIdMunicipality();
+            ViewBag.logo = municipalidad.Nombre;
+            ViewBag.cementerio = municipalidad.Cementerio;
+            Gasto_Ano gasto = db.Gasto_Ano.Find(id);
+            Personal_Ano ingr = db.Personal_Ano.First(r => r.Ano == gasto.Ano && r.Semestre == gasto.Semestre && r.IdMunicipalidad == gasto.IdMunicipalidad);
+            ViewBag.aviso = ingr.Cargado;
+            switch (ingr.Semestre)
+            {
+                case 1:
+                    ViewBag.ano = ingr.Ano + "a marzo";
+                    break;
+                case 2:
+                    ViewBag.ano = ingr.Ano + "a junio";
+                    break;
+                case 3:
+                    ViewBag.ano = ingr.Ano + "a septiembre";
+                    break;
+                default:
+                    ViewBag.ano = ingr.Ano;
+                    break;
+            }
             return View();
         }
     }
