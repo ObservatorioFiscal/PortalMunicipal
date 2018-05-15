cedcb4cd8d13cbc8f125022de16f7282ff233df4
gpradena <gpradena@metricarts.com>
2018-05-15 17:22:11 -0400

    update

--- GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
+++ GastoTransparenteMunicipal/GastoTransparenteMunicipal/Controllers/AdminComunaController.cs
@@ -125,6 +125,8 @@ namespace GastoTransparenteMunicipal.Controllers
             }
             return View();
         }
+
+        #region Proveedores
         public ActionResult CargaProveedores(int id)
         {
             var municipalidad = GetCurrentIdMunicipality();
@@ -150,6 +152,175 @@ namespace GastoTransparenteMunicipal.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaProveedoresAdm(HttpPostedFileBase admServicios)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();          
+                using (Stream fileStream = admServicios.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresAdmServicios(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaProveedoresSalud(HttpPostedFileBase salud)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+
+                using (Stream fileStream = salud.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresSalud(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }            
+        }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaProveedoresEducacion(HttpPostedFileBase educacion)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+                using (Stream fileStream = educacion.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresEducacion(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaProveedoresCementerio(HttpPostedFileBase cementerio)
+        {
+            bool isValid = true;
+            try
+            { 
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+                using (Stream fileStream = cementerio.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresCementerio(xssfwb);
+                }
+                return Json(isValid);
+            }
+
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }            
+        }
+
+        [HttpPost]
+        public ActionResult CargaProveedores(HttpPostedFileBase admServicios, HttpPostedFileBase salud, HttpPostedFileBase educacion, HttpPostedFileBase cementerio)
+        {
+            XSSFWorkbook xssfwb;
+
+            int idMunicipality = GetCurrentIdMunicipality().IdMunicipalidad;
+            int year = 2017;
+            int month = 0;
+            LoadReport loadReport = new LoadReport();
+            Proveedor_Ano proveedorAno = new Proveedor_Ano { IdMunicipalidad = idMunicipality, Ano = year, Semestre = month, UpdatedOn = DateTime.Now };
+            if (admServicios != null || salud != null || educacion != null || cementerio != null)
+            {
+                db.Proveedor_Ano.Add(proveedorAno);
+                db.SaveChanges();
+            }
+            else
+            {
+                return View();
+            }
+
+            if (admServicios != null)
+            {
+                using (Stream fileStream = admServicios.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+
+                    var result = loadReport.LoadInformeProveedoresAdmServicios(xssfwb);
+                    db.Proveedor_AdmInforme.AddRange(result);
+                    db.SaveChanges();
+
+                    db.SP_ProveedorAdm(loadReport.IdGroupInforme, proveedorAno.IdAno);
+                }
+            }
+
+            if (salud != null)
+            {
+                using (Stream fileStream = salud.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresSalud(xssfwb);
+                    db.Proveedor_SaludInforme.AddRange(result);
+                    db.SaveChanges();
+
+                    db.SP_ProveedorSalud(loadReport.IdGroupInforme, proveedorAno.IdAno);
+                }
+            }
+
+            if (educacion != null)
+            {
+                using (Stream fileStream = educacion.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresEducacion(xssfwb);
+                    db.Proveedor_EducacionInforme.AddRange(result);
+                    db.SaveChanges();
+
+                    db.SP_ProveedorEducacion(loadReport.IdGroupInforme, proveedorAno.IdAno);
+                }
+            }
+
+            if (cementerio != null)
+            {
+                using (Stream fileStream = cementerio.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformeProveedoresCementerio(xssfwb);
+                    db.Proveedor_CementerioInforme.AddRange(result);
+                    db.SaveChanges();
+
+                    db.SP_ProveedorCementerio(loadReport.IdGroupInforme, proveedorAno.IdAno);
+                }
+
+            }
+
+            db.SaveChanges();
+            db.SP_ProveedorTotal(loadReport.IdGroupInforme, proveedorAno.IdAno);
+
+            return View();
+        }
+        #endregion
+
+        #region Subsidios
         public ActionResult CargaSubsidios(int id)
         {
             var municipalidad = GetCurrentIdMunicipality();
@@ -175,6 +346,53 @@ namespace GastoTransparenteMunicipal.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaSubsidios(HttpPostedFileBase file)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                using (Stream fileStream = file.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    LoadReport loadReport = new LoadReport();
+                    var result = loadReport.LoadInformeSubsidio(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch
+            {
+                return Json(!isValid);
+            }                       
+        }
+
+        [HttpPost]
+        public ActionResult CargaSubsidios(HttpPostedFileBase file)
+        {
+            XSSFWorkbook xssfwb;
+            int idMunicipality = GetCurrentIdMunicipality().IdMunicipalidad;
+            int year = 2017;
+            int month = 0;
+            Subsidio_Ano subsidioAno = new Subsidio_Ano { IdMunicipalidad = idMunicipality, Ano = year, Semestre = month, UpdatedOn = DateTime.Now };
+
+            using (Stream fileStream = file.InputStream)
+            {
+                xssfwb = new XSSFWorkbook(fileStream);
+                LoadReport loadReport = new LoadReport();
+                var result = loadReport.LoadInformeSubsidio(xssfwb);
+                db.SubsidioInforme.AddRange(result);
+                db.Subsidio_Ano.Add(subsidioAno);
+                db.SaveChanges();
+
+                db.SP_InformeSubsidio(loadReport.IdGroupInforme, subsidioAno.IdAno);
+            }
+            return View();
+        }
+        #endregion
+
+        #region Corporaciones
         public ActionResult CargaCorporaciones(int id)
         {
             var municipalidad = GetCurrentIdMunicipality();
@@ -200,6 +418,54 @@ namespace GastoTransparenteMunicipal.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaCorporaciones(HttpPostedFileBase file)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                using (Stream fileStream = file.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    LoadReport loadReport = new LoadReport();
+                    var result = loadReport.LoadInformeCorporaciones(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }            
+        }
+
+        [HttpPost]
+        public ActionResult CargaCorporaciones(HttpPostedFileBase file)
+        {
+            XSSFWorkbook xssfwb;
+            int idMunicipality = GetCurrentIdMunicipality().IdMunicipalidad;
+            int year = 2017;
+            int month = 0;
+
+            DateTime data = DateTime.Now;
+            Corporacion_Ano corporacionAno = new Corporacion_Ano { IdMunicipalidad = idMunicipality, Ano = year, Semestre = month, UpdatedOn = DateTime.Now };
+            using (Stream fileStream = file.InputStream)
+            {
+                xssfwb = new XSSFWorkbook(fileStream);
+                LoadReport loadReport = new LoadReport();
+                var result = loadReport.LoadInformeCorporaciones(xssfwb);
+                db.CorporacionInforme.AddRange(result);
+                db.Corporacion_Ano.Add(corporacionAno);
+                db.SaveChanges();
+
+                db.SP_InformeCorporaciones(loadReport.IdGroupInforme, corporacionAno.IdAno);
+            }
+            return View();
+        }
+        #endregion
+
+        #region Remuneraciones
         public ActionResult CargaRemuneraciones(int id)
         {
             var municipalidad = GetCurrentIdMunicipality();
@@ -225,6 +491,176 @@ namespace GastoTransparenteMunicipal.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaRemuneracionesAdm(HttpPostedFileBase admServicios)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+                using (Stream fileStream = admServicios.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalAdmServicios(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaRemuneracionesSalud(HttpPostedFileBase salud)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+
+                using (Stream fileStream = salud.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalSalud(xssfwb);
+                }
+
+                return Json(isValid);
+            }
+            catch (Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ValidadorCargaRemuneracionesEducacion(HttpPostedFileBase educacion)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+                using (Stream fileStream = educacion.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalEducacion(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ValidadorCargaRemuneracionesCementerio(HttpPostedFileBase cementerio)
+        {
+            bool isValid = true;
+            try
+            {
+                XSSFWorkbook xssfwb;
+                LoadReport loadReport = new LoadReport();
+
+                using (Stream fileStream = cementerio.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalCementerio(xssfwb);
+                }
+                return Json(isValid);
+            }
+            catch(Exception ex)
+            {
+                return Json(!isValid);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CargaRemuneraciones(HttpPostedFileBase admServicios, HttpPostedFileBase salud, HttpPostedFileBase educacion, HttpPostedFileBase cementerio)
+        {
+            XSSFWorkbook xssfwb;
+
+            int idMunicipality = GetCurrentIdMunicipality().IdMunicipalidad;
+            int year = 2017;
+            int month = 0;
+
+            DateTime data = DateTime.Now;
+            LoadReport loadReport = new LoadReport();
+            Personal_Ano personalAno = new Personal_Ano { IdMunicipalidad = idMunicipality, Ano = year, Semestre = month, UpdatedOn = DateTime.Now };
+
+            if (admServicios != null || salud != null || educacion != null || cementerio != null)
+            {
+                db.Personal_Ano.Add(personalAno);
+                db.SaveChanges();
+            }
+            else
+            {
+                return View();
+            }
+
+            if (admServicios != null)
+            {
+                using (Stream fileStream = admServicios.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+
+                    var result = loadReport.LoadInformePersonalAdmServicios(xssfwb);
+                    db.Personal_AdmInforme.AddRange(result);
+
+                    db.SaveChanges();
+                    db.SP_InformePersonalAdm(loadReport.IdGroupInforme, personalAno.IdAno);
+                }
+            }
+
+            if (salud != null)
+            {
+                using (Stream fileStream = salud.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalSalud(xssfwb);
+                    db.Personal_SaludInforme.AddRange(result);
+
+                    db.SaveChanges();
+                    db.SP_InformePersonalSalud(loadReport.IdGroupInforme, personalAno.IdAno);
+                }
+            }
+
+            if (educacion != null)
+            {
+                using (Stream fileStream = educacion.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalEducacion(xssfwb);
+                    db.Personal_EducacionInforme.AddRange(result);
+
+                    db.SaveChanges();
+                    db.SP_InformePersonalEducacion(loadReport.IdGroupInforme, personalAno.IdAno);
+                }
+            }
+
+            if (cementerio != null)
+            {
+                using (Stream fileStream = cementerio.InputStream)
+                {
+                    xssfwb = new XSSFWorkbook(fileStream);
+                    var result = loadReport.LoadInformePersonalCementerio(xssfwb);
+                    db.Personal_CementerioInforme.AddRange(result);
+
+                    db.SaveChanges();
+                    db.SP_InformePersonalCementerio(loadReport.IdGroupInforme, personalAno.IdAno);
+                }
+            }
+
+            db.SP_InformePersonalMunicipioTotal(loadReport.IdGroupInforme, personalAno.IdAno);
+
+            return View();
+        }
+        #endregion
+
     }
 
 }
